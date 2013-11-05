33cad7e5cae860c1ef657784309e773d8d204c8f
John Kabler <johnkabler@localhost.local>
2013-11-05 18:08:55 -0500

    Added basic routing

---
+++ client/helpers/router.js
@@ -0,0 +1,6 @@
+Meteor.Router.add({
+	'/' : 'postsList'
+});
+//By default the name of the route here is "postsList"
+//This allows us to start using convenience functions from the Router package
+//Check the navbar href which takes postsListPath to bring us back to /
