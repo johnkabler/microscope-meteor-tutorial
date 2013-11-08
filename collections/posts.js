9d3f8b9615de1273b9d906c8e6b4154ff678e363
John Kabler <johnkabler@localhost.local>
2013-11-08 11:42:00 -0500

    removed the default insecure package, did a basic allow for certain posts

--- collections/posts.js
+++ collections/posts.js
@@ -1,3 +1,14 @@
 //Notice how there is no var in front of Posts
 //This is because it will be available to all files, while var would limit its scope
 Posts = new Meteor.Collection('posts');
+
+//This is a temporary method for allowing inserts.
+//We will refine this to something better later.  
+//For now this will work
+
+Posts.allow({
+	insert: function(userId, doc) {
+		//only allow posting if you are logged in;
+		return !! userId;
+	}
+});
