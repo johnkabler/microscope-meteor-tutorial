88790f5f2c8144695909764e890154228549896f
John Kabler <johnkabler@localhost.local>
2013-11-08 12:32:33 -0500

    Added an denial of acess to new posts when user is not logged in

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -14,3 +14,14 @@ Meteor.Router.add({
 //By default the name of the route here is "postsList"
 //This allows us to start using convenience functions from the Router package
 //Check the navbar href which takes postsListPath to bring us back to /
+
+Meteor.Router.filters({
+	'requireLogin': function(page) {
+		if (Meteor.user())
+			return page;
+		else 
+			return 'accessDenied';
+	}
+});
+
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
