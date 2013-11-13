155e12fb592d61b980dd135b2ecad7a652843012
John Kabler <johnkabler@new-host-4.home>
2013-11-12 23:01:51 -0500

    Monitor what errors have been seen, clear when routed

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -28,7 +28,12 @@ Meteor.Router.filters({
 			return 'loading';
 		else 
 			return 'accessDenied';
+	}, 
+	'clearErrors': function(page) {
+		clearErrors();
+		return page;
 	}
 });
 
 Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
+Meteor.Router.filter('clearErrors');
