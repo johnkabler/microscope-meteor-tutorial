0d084d483a69a8227fb80280d828c86627ff9d0b
John Kabler <johnkabler@new-host-2.home>
2013-11-18 22:20:51 -0500

    Use the paginated subscription to step through posts

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
-Meteor.subscribeWithPagination('posts', 10); //Modified to work with paginated-subscription
+//notice we are adding a global variable
+postsHandle = Meteor.subscribeWithPagination('posts', 10); //Modified to work with paginated-subscription
 //Tell the client to subscribe to the servers published 'posts' Cursor
 Meteor.autorun(function() {
 	Meteor.subscribe('comments', Session.get('currentPostId'));
--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,6 +1,23 @@
 
 Template.postsList.helpers({
 	posts: function() {
-		return Posts.find({}, {sort: {submitted: -1}});
+		return Posts.find({}, {sort: {submitted: -1}, limit: postsHandle.limit()}
+		);
+	},
+
+	postsReady: function() {
+		return ! postsHandle.loading();
+	},
+
+	allPostsLoaded: function() {
+		return ! postsHandle.loading() && 
+			Posts.find().count() < postsHandle.loaded();
+	}
+});
+
+Template.postsList.events({
+	'click .load-more': function(event) {
+		event.preventDefault();
+		postsHandle.loadNextPage();
 	}
 });
