3bb2303194b60d5ef63b81fe699cd34d86bfcba3
John Kabler <johnkabler@localhost.local>
2013-11-25 16:00:26 -0500

    Added basic upvoting functionality

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,28 +1,23 @@
 if (Posts.find().count() === 0) {
 	var now = new Date().getTime();
-
-	//create two users
+	// create two users
 	var tomId = Meteor.users.insert({
 		profile: { name: 'Tom Coleman' }
 	});
-
 	var tom = Meteor.users.findOne(tomId);
-	
 	var sachaId = Meteor.users.insert({
 		profile: { name: 'Sacha Greif' }
 	});
-
 	var sacha = Meteor.users.findOne(sachaId);
-
 	var telescopeId = Posts.insert({
 		title: 'Introducing Telescope',
 		userId: sacha._id,
 		author: sacha.profile.name,
 		url: 'http://sachagreif.com/introducing-telescope/',
-		submitted: now - 7 * 3600 * 1000 ,
-		commentsCount: 2
+		submitted: now - 7 * 3600 * 1000,
+		commentsCount: 2,
+		upvoters: [], votes: 0
 	});
-
 	Comments.insert({
 		postId: telescopeId,
 		userId: tom._id,
@@ -37,31 +32,33 @@ if (Posts.find().count() === 0) {
 		submitted: now - 3 * 3600 * 1000,
 		body: 'You sure can Tom!'
 	});
-
 	Posts.insert({
 		title: 'Meteor',
 		userId: tom._id,
 		author: tom.profile.name,
 		url: 'http://meteor.com',
-		submitted: now - 10 * 3600 * 1000 ,
-		commentsCount: 0
-	});
-	Posts.insert({
+		submitted: now - 10 * 3600 * 1000,
+		commentsCount: 0,
+		upvoters: [], votes: 0
+		});
+		Posts.insert({
 		title: 'The Meteor Book',
 		userId: tom._id,
 		author: tom.profile.name,
 		url: 'http://themeteorbook.com',
-		submitted: now - 12 * 3600 * 1000 ,
-		commentsCount: 0
+		submitted: now - 12 * 3600 * 1000,
+		commentsCount: 0,
+		upvoters: [], votes: 0
 	});
 	for (var i = 0; i < 10; i++) {
 		Posts.insert({
-			title: 'Test post #' + i,
-			author: sacha.profile.name,
-			userId: sacha._id,
-			url: 'http://google.com/?q=test-' + i,
-			submitted: now - i * 3600 * 1000,
-			commentsCount: 0
-		});
+		title: 'Test post #' + i,
+		author: sacha.profile.name,
+		userId: sacha._id,
+		url: 'http://google.com/?q=test-' + i,
+		submitted: now - i * 3600 * 1000,
+		commentsCount: 0,
+		upvoters: [], votes: 0
+	});
 	}
 }
