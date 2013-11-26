46b1ed9d9a134ce45f48fb598a8afe48e9b52fe0
John Kabler <johnkabler@localhost.local>
2013-11-26 18:08:45 -0500

    Added two pubs for posts, along with templates for display

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 //notice we are adding a global variable
-postsHandle = Meteor.subscribeWithPagination('newPosts', 10); //Modified to work with paginated-subscription
+newPostsHandle = Meteor.subscribeWithPagination('newPosts', 10); //Modified to work with paginated-subscription
 //Tell the client to subscribe to the servers published 'posts' Cursor
+topPostsHandle = Meteor.subscribeWithPagination('topPosts', 10);
+
 Meteor.autorun(function() {
 	Meteor.subscribe('singlePost', Session.get('currentPostId'));
 
