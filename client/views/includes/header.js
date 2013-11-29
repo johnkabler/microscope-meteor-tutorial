3f74acbd6be02cb729f57418b4f47cc8c1670c6d
John Kabler <johnkabler@localhost.local>
2013-11-29 11:08:43 -0500

    Added animations for post reordering

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -1,6 +1,11 @@
 Template.header.helpers({
 	activeRouteClass: function(/* route names */) {
 		var args = Array.prototype.slice.call(arguments, 0);
+		//The above bit of magic is done because 'arguments' isn't an array
+		//it is an array like object, and therefore is set to 'this' in the 
+		//first argument
+		//http://stackoverflow.com/questions/7056925/how-does-array-prototype-slice-call-work
+		//above is relevant stack overflow article
 		args.pop();
 
 		var active = _.any(args, function(name) {
--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -18,10 +18,19 @@ Template.bestPosts.helpers({
 
 
 Template.postsList.helpers({
-	posts: function() {
-		return Posts.find({}, {sort: this.sort, limit: this.handle.limit()}
-		);
+	postsWithRank: function() {
+		var i = 0, options = {sort: this.sort, limit: this.handle.limit()};
+		return Posts.find({}, options).map(function(post){
+			post._rank = i;
+			i += 1;
+			return post;
+		});
+
 	},
+	// posts: function() {
+	// 	return Posts.find({}, {sort: this.sort, limit: this.handle.limit()}
+	// 	);
+	// },
 
 	postsReady: function() {
 		return this.handle.ready();
