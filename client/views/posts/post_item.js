106a38a5c95ba86518bf6c5b6057873cedd2a84a
John Kabler <johnkabler@localhost.local>
2013-11-29 11:59:24 -0500

    Added in final new post animation

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -20,10 +20,21 @@ Template.postItem.helpers({
 });
 
 Template.postItem.rendered = function() {
+	//remember, items can't be moved in the DOM, only deleted and created
+
+	// this means in order to animate, we have to animate the recreated element
+	// to move from its former position to its new position on top of another element
+	//it has been newly ranked above.
+
+	//Due to Meteor's reactivity, the creation and deletion of a DOM element
+	//happens automatically, as soon as data behind the DOM element changes
+
+
 	// animate from previous position to new position
 	var instance = this;
 	var rank = instance.data._rank;
-	var $this = $(this.firstNode);
+	var $this = $(this.firstNode); //this.firstNode is part of template api
+	//it accesses the first top level dom node in the current template instance
 	var postHeight = 80;
 	var newPosition = rank * postHeight;
 
@@ -34,12 +45,15 @@ Template.postItem.rendered = function() {
 		// send element there
 		var delta = previousPosition - newPosition;
 		$this.css("top", delta + "px");
+	} else {
+		// it's the first ever render, so hide element until the defer kicks off
+		$this.addClass("invisible");
 	}
 	//let it draw in the old position then
 	Meteor.defer(function() {
 		instance.currentPosition = newPosition;
 		// bring element back to its new original position
-		$this.css("top", "0px");
+		$this.css("top", "0px").removeClass("invisible");
 	});
 };
 
